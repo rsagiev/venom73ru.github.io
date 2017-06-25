994679a4f07ad003c3196b06c04ed77721f26656
Сагиев Роман <venom73ru@yandex.ru>
2017-06-25 18:34:33 +0400

    Правка мобильных стилей, изменил фон, поправил скрипт скролла

--- js/script.js
+++ js/script.js
@@ -1,31 +1,19 @@
 $(document).ready(function () {
     $(document).on("scroll", onScroll);
-    //Наведение мышью
-   /* $('ul li').hover(function() {
-    	console.log(linePos);
-		$('.menuUnderline').stop().animate({
-			'left' : $(this).position().left
-		}, 400);
-	}, function(){
-		$('.menuUnderline').stop().animate({
-			'left' : linePos
-		}, 400);
-    });
-*/
+
 	$('#butt a').on('click', function(){
 		$('.#').stop().animate({
 			'left' : $(this).position().left
 		}, 400);
 	});
     //Скролл по якорям
-    $('a[href^="#"]').on('click', function (e) {
+    $('#down').on('click', function (e) {
         e.preventDefault();
         $(document).off("scroll");
         $('.menuUnderline').stop().animate({
 				'left' : $(this).position().left
-			}, 2000, 'swing', console.log($(this).position().left));
+			}, 500, 'swing', console.log($(this).position().left));
 
-      
         var target = this.hash,
             menu = target;
         $target = $(target);
@@ -36,7 +24,25 @@ $(document).ready(function () {
             $(document).on("scroll", onScroll);
         });
     });
+    $('#up').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        $('.menuUnderline').stop().animate({
+                'left' : 0
+            }, 500, 'swing', console.log($(this).position().left));
 
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': 0 - 48
+        }, 300, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+
+    //Переключение мобильного меню
     $('.bars').click(function(){
         $('#menu').toggleClass('open');
     });
@@ -60,54 +66,7 @@ $(document).ready(function () {
 			$('#header').addClass('black');
 		}
 		else{
-			console.log(scrollPos);
 			$('#header').removeClass('black');
 		}
 
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*function scrollNav() {
-  $('.nav a').click(function(){  
-    //Toggle Class
-    $(".active").removeClass("active");      
-    $(this).closest('li').addClass("active");
-    var theClass = $(this).attr("class");
-    $('.'+theClass).parent('li').addClass('active');
-    //Animate
-    $('html, body').stop().animate({
-        scrollTop: $( $(this).attr('href') ).offset().top - 60
-    }, 400);
-    return false;
-  });
-}
-$('ul li').hover(function() {
-		var x = $(this);
-		$('.menuUnderline').stop().animate({
-			'width': x.width(),
-			'left' : x.position().left
-		}, 400);
-	}, function(){
-        var x = $(this);
-		$('.menuUnderline').stop().animate({
-			'width': 0,
-			'left' : 0
-		}, 400);
-    });
-
-window.onload = function() {
-	scrollMenu();
-    scrollNav();
-};*/
